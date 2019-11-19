c266a33941f588b901a9890d2fa86ec02f858a9c
Aaramitechs <aaramitechsolutions@gmail.com>
2019-11-19 12:04:40 +0530

    changes to be Committed  to ATS_1_br

---
+++ .sfdx/typings/lwc/customlabels.d.ts
@@ -0,0 +1,32 @@
+declare module "@salesforce/label/c.Campaign_InProgress_Status" {
+    var Campaign_InProgress_Status: string;
+    export default Campaign_InProgress_Status;
+}
+declare module "@salesforce/label/c.Campaign_Not_Found_Error_Msg" {
+    var Campaign_Not_Found_Error_Msg: string;
+    export default Campaign_Not_Found_Error_Msg;
+}
+declare module "@salesforce/label/c.Octopus_Activity_Completed_Status" {
+    var Octopus_Activity_Completed_Status: string;
+    export default Octopus_Activity_Completed_Status;
+}
+declare module "@salesforce/label/c.Octopus_Activity_Default_Contact" {
+    var Octopus_Activity_Default_Contact: string;
+    export default Octopus_Activity_Default_Contact;
+}
+declare module "@salesforce/label/c.Octopus_Activity_Initial_Status" {
+    var Octopus_Activity_Initial_Status: string;
+    export default Octopus_Activity_Initial_Status;
+}
+declare module "@salesforce/label/c.Octopus_Campaign_Member_Status" {
+    var Octopus_Campaign_Member_Status: string;
+    export default Octopus_Campaign_Member_Status;
+}
+declare module "@salesforce/label/c.Octopus_Campaign_Not_found_Error" {
+    var Octopus_Campaign_Not_found_Error: string;
+    export default Octopus_Campaign_Not_found_Error;
+}
+declare module "@salesforce/label/c.Octopus_Sync_Permitted_Profiles" {
+    var Octopus_Sync_Permitted_Profiles: string;
+    export default Octopus_Sync_Permitted_Profiles;
+}
